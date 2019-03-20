9d4abee349c84e0080505e02af7fc018040a79e4
AnnPla <anna.platek2222@gmail.com>
2019-03-20 21:33:48 +0100

    Tests with list, tuple and input

--- fiszka_script.py
+++ fiszka_script.py
@@ -3,7 +3,16 @@
 
 import random
 
-world_eng='hi'
-world_pl='cześć'
+n=1
+word_eng='hi'
+word_pl='cześć'
 
-print(world_eng, world_pl)
+print(n, word_eng, word_pl)
+list=[n,word_eng,word_pl]
+print('list',list)
+
+tuple=(n,word_eng,word_pl)
+print(tuple)
+
+user_word_eng=input("Please, put word in English: ")
+user_word_pl=input("Please, put this worf in Polish: ")
